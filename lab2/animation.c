199eb2bd90bef6cc54de82d54e607a09f05da4bb
Nat Su <nathansupf@gmail.com>
2026-09-24 09:44:09 -0400

    working w dma in prog

--- lab2/animation.c
+++ lab2/animation.c
@@ -56,6 +56,8 @@
 #include "hardware/dma.h"
 #include "hardware/clocks.h"
 #include "hardware/pll.h"
+#include "hardware/spi.h"
+
 // Include protothreads
 #include "pt_cornell_rp2040_v1_4.h"
 
@@ -81,6 +83,32 @@ typedef signed int fix15 ;
 #define ENC_B  11
 #define ENC_SW 12
 #define ENC_SW_DEBOUNCE_US 20000 // 20 ms
+
+// Number of samples per period in sine table
+#define sine_table_size 256
+
+// Sine table
+int raw_sin[sine_table_size] ;
+
+// Table of values to be sent to DAC
+unsigned short DAC_data[sine_table_size] ;
+
+// Pointer to the address of the DAC data table
+unsigned short * address_pointer = &DAC_data[0] ;
+
+// A-channel, 1x, active
+#define DAC_config_chan_A 0b0011000000000000
+
+//SPI configurations
+#define PIN_MISO 4
+#define PIN_CS   5
+#define PIN_SCK  6
+#define PIN_MOSI 7
+#define SPI_PORT spi0
+
+// Number of DMA transfers per event
+const uint32_t transfer_count = sine_table_size ;
+
 
 volatile int enc_count = 0; // number shown on VGA
 static volatile uint8_t enc_state; // state of pins A and B written in last 2 bits as AB
@@ -176,6 +204,74 @@ fix15 bounciness = float2fix15(0.5) ;
 // Create a semaphore
 semaphore_t draw_semaphore ;
 
+static void dmaSetup(void) {
+  // Initialize SPI channel (channel, baud rate set to 20MHz)
+    spi_init(SPI_PORT, 20000000) ;
+
+    // Format SPI channel (channel, data bits per transfer, polarity, phase, order)
+    spi_set_format(SPI_PORT, 16, 0, 0, 0);
+
+    // Map SPI signals to GPIO ports, acts like framed SPI with this CS mapping
+    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_CS, GPIO_FUNC_SPI) ;
+    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+
+    // Build sine table and DAC data table
+    int i ;
+    for (i=0; i<(sine_table_size); i++){
+        raw_sin[i] = (int)(2047 * sin((float)i*6.283/(float)sine_table_size) + 2047); //12 bit
+        DAC_data[i] = DAC_config_chan_A | (raw_sin[i] & 0x0fff) ;
+    }
+
+    // Select DMA channels
+    int data_chan = dma_claim_unused_channel(true);;
+    int ctrl_chan = dma_claim_unused_channel(true);;
+
+    // Setup the control channel
+    dma_channel_config c = dma_channel_get_default_config(ctrl_chan);   // default configs
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_32);             // 32-bit txfers
+    channel_config_set_read_increment(&c, false);                       // no read incrementing
+    channel_config_set_write_increment(&c, false);                      // no write incrementing
+    channel_config_set_chain_to(&c, data_chan);                         // chain to data channel
+
+    dma_channel_configure(
+        ctrl_chan,                          // Channel to be configured
+        &c,                                 // The configuration we just created
+        &dma_hw->ch[data_chan].read_addr,   // Write address (data channel read address)
+        &address_pointer,                   // Read address (POINTER TO AN ADDRESS)
+        1,                                  // Number of transfers
+        false                               // Don't start immediately
+    );
+
+    // Setup the data channel
+    dma_channel_config c2 = dma_channel_get_default_config(data_chan);  // Default configs
+    channel_config_set_transfer_data_size(&c2, DMA_SIZE_16);            // 16-bit txfers
+    channel_config_set_read_increment(&c2, true);                       // yes read incrementing
+    channel_config_set_write_increment(&c2, false);                     // no write incrementing
+    // (X/Y)*sys_clk, where X is the first 16 bytes and Y is the second
+    // sys_clk is 125 MHz unless changed in code. Configured to ~44 kHz
+    dma_timer_set_fraction(0, 0x0017, 0xffff) ;
+    // 0x3b means timer0 (see SDK manual)
+    channel_config_set_dreq(&c2, 0x3b);                                 // DREQ paced by timer 0
+    // chain to the controller DMA channel
+    channel_config_set_chain_to(&c2, ctrl_chan);                        // Chain to control channel
+
+
+    dma_channel_configure(
+        data_chan,                  // Channel to be configured
+        &c2,                        // The configuration we just created
+        &spi_get_hw(SPI_PORT)->dr,  // write address (SPI data register)
+        DAC_data,                   // The initial read address
+        sine_table_size,            // Number of transfers
+        false                       // Don't start immediately.
+    );
+
+
+  // start the control channel
+  dma_start_channel_mask(1u << ctrl_chan) ;
+}
+
 // Create a boid
 void spawnBoid(fix15* x, fix15* y, fix15* vx, fix15* vy, int direction)
 {
@@ -183,7 +279,7 @@ void spawnBoid(fix15* x, fix15* y, fix15* vx, fix15* vy, int direction)
 
   // Start in top center of screen instead of center
   *x = int2fix15(320) ;
-  *y = int2fix15(20) ;
+  *y = int2fix15(30) ;
 
   // Randomized horizontal velocity
   // float random_vx = -0.2f + ((float)rand() / 100) * 0.6f;
@@ -215,14 +311,14 @@ void wallsAndEdges(fix15* x, fix15* y, fix15* vx, fix15* vy)
   //   *vy = (-*vy) ;
   //   *y  = (*y - int2fix15(5)) ;
   // } 
-  if (hitRight(*x)) {
-    *vx = (-*vx) ;
-    *x  = (*x - int2fix15(5)) ;
-  }
-  if (hitLeft(*x)) {
-    *vx = (-*vx) ;
-    *x  = (*x + int2fix15(5)) ;
-  } 
+  // if (hitRight(*x)) {
+  //   *vx = (-*vx) ;
+  //   *x  = (*x - int2fix15(5)) ;
+  // }
+  // if (hitLeft(*x)) {
+  //   *vx = (-*vx) ;
+  //   *x  = (*x + int2fix15(5)) ;
+  // } 
 
   // Update position using velocity
   *x = *x + *vx ;
@@ -379,6 +475,8 @@ int main(){
   set_sys_clock_khz(150000, true) ;
   // initialize stio
   stdio_init_all() ;
+
+  dmaSetup();
 
   // initialize rotary encoder
   enc_init();
